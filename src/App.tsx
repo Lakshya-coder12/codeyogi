b9f53a691736b430920dfbb6e1908e65674b5f89
Lakshya Kumar <lkkumar544@gmail.com>
2021-07-15 00:20:19 +0530

    added routes for AppContainer AuthPage and NotFound components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import AppContainerPage from "./pages/AppContainer.page";
+import AuthPage from "./pages/Auth.page";
+import NotFoundPage from "./pages/NotFound.page";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/login" />
+        </Route>
+        <Route path={["/login", "/signup"]} exact>
+          <AuthPage />
+        </Route>
+        <Route
+          path={[
+            "/dashboard",
+            "/recordings",
+            "/batch/:batchNumber/lecture/:lectureNumber",
+          ]}
+          exact
+        >
+          <AppContainerPage />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
