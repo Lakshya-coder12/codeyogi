6cad3cbd709932b84764c06845d26bc8d72865ad
Lakshya Kumar <lkkumar544@gmail.com>
2021-07-18 10:00:13 +0530

    changed color of placeholder

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -16,17 +16,17 @@ const Input: React.FC<Props> = ({
   ...rest
 }) => {
   return (
-    <div className={"pt-3 pb-6 " + className}>
+    <div className={"pt-2.75 pb-6.25 " + className}>
       {id && placeholder && (
         <label htmlFor={id} className="sr-only">
           {placeholder}
         </label>
       )}
-      <div className="flex items-center pb-3 border-b border-gray-200">
+      <div className="flex items-center pb-2.5 border-b border-gray-200">
         {svg}
         <input
           {...rest}
-          className="w-full pl-4 outline-none"
+          className="w-full pl-4 font-semibold placeholder-gray-300 outline-none"
           id={id}
           placeholder={placeholder}
         ></input>
