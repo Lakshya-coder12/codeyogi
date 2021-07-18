9df510857b1b71146da2891c9b4be37ab8165176
Lakshya Kumar <lkkumar544@gmail.com>
2021-07-18 09:59:28 +0530

    added new margin,padding and font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        40: "2.5rem",
+      },
+      margin: {
+        12.5: "3.125rem",
+        22.5: "5.625rem",
+      },
+      padding: {
+        2.75: "0.688rem",
+        6.25: "1.563rem",
+        2.5: "0.625rem",
+      },
+    },
   },
   variants: {
     extend: {},
