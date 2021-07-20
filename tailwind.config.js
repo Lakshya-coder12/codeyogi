6999d7358fff5ba8d888f080a50fe5c2f4f3b696
Lakshya Kumar <lkkumar544@gmail.com>
2021-07-20 18:41:59 +0530

    added new background color, textSize, padding classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundColor: {
+        navbar: "#0e1726",
+        header: "#fafafa",
+        sidebar: "#f1f2f3",
+      },
       fontSize: {
         40: "2.5rem",
+        13: "0.813rem",
       },
       margin: {
         12.5: "3.125rem",
@@ -14,6 +20,7 @@ module.exports = {
         2.75: "0.688rem",
         6.25: "1.563rem",
         2.5: "0.625rem",
+        3.5: "0.875rem",
       },
     },
   },
