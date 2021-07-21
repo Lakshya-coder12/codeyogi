aae8e3e78b4104071f1f2f2d592e81192da2b024
Lakshya Kumar <lkkumar544@gmail.com>
2021-07-21 10:59:03 +0530

    added story for Input component

---
+++ src/components/Input/Input.stories.tsx
@@ -0,0 +1,26 @@
+import Input from "./Input";
+import { FiLock, FiAtSign, FiUser } from "react-icons/fi";
+
+const icons = { FiLock, FiAtSign, FiUser };
+
+export default {
+  title: "Input",
+  component: Input,
+  argTypes: {
+    Icon: {
+      options: Object.keys(icons),
+      mapping: icons,
+      control: {
+        type: "select",
+      },
+    },
+  },
+};
+
+export const main = (args: any) => <Input {...args}></Input>;
+
+main.args = {
+  placeholder: "Username",
+  touched: "false",
+  error: "enter valid data",
+};
