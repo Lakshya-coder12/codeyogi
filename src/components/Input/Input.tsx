bc81178aadbc077448f1529f2fc3ae180a38c3c1
Lakshya Kumar <lkkumar544@gmail.com>
2021-07-21 16:00:30 +0530

    resolved problem of scroll due to errors shown in input field

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -23,16 +23,20 @@ const Input: React.FC<Props> = ({
           {placeholder}
         </label>
       )}
-      <div className="flex items-center pb-2.5 border-b border-gray-200 focus-within:border-blue-600">
-        {Icon && <Icon className="w-6 h-6 text-blue-600"></Icon>}
-        <input
-          {...rest}
-          className="w-full pl-4 font-semibold placeholder-gray-300 outline-none"
-          id={id}
-          placeholder={placeholder}
-        ></input>
+      <div className="relative">
+        <div className="flex items-center pb-2.5 border-b border-gray-200 focus-within:border-blue-600">
+          {Icon && <Icon className="w-6 h-6 text-blue-600"></Icon>}
+          <input
+            {...rest}
+            className="w-full pl-4 font-semibold placeholder-gray-300 outline-none"
+            id={id}
+            placeholder={placeholder}
+          ></input>
+        </div>
+        {touched && (
+          <div className="absolute left-0 text-red-500 top-8">{error}</div>
+        )}
       </div>
-      {touched && <div className="text-red-500">{error}</div>}
     </div>
   );
 };
