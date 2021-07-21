b82a04a9f959b6652655bc06a645161596b0e2ea
Lakshya Kumar <lkkumar544@gmail.com>
2021-07-21 11:05:49 +0530

    added component for Progress Bar

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+interface Props {
+  width: number;
+  theme?: "blue" | "red" | "green" | "black" | "yellow";
+}
+
+const ProgressBar: React.FC<Props> = ({ width, theme }) => {
+  let themeClass = "";
+  if (width > 100) {
+    width = 100;
+  }
+  console.log(theme);
+  if (theme === "blue") {
+    themeClass = "bg-blue-600";
+  } else if (theme === "red") {
+    themeClass = "bg-red-600";
+  } else if (theme === "green") {
+    themeClass = "bg-green-600";
+  } else if (theme === "black") {
+    themeClass = "bg-black";
+  } else if (theme === "yellow") {
+    themeClass = "bg-yellow-600";
+  }
+  console.log(themeClass);
+  return (
+    <div className="mx-4">
+      <div className="w-full h-4 bg-gray-200 rounded-full">
+        <div
+          className={"h-full rounded-full " + themeClass}
+          style={{ width: `${width}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+ProgressBar.defaultProps = {
+  theme: "blue",
+};
+
+export default React.memo(ProgressBar);
