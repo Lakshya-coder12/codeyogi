24486f277fd619db62b2d13f281515958cab5a2c
Lakshya Kumar <lkkumar544@gmail.com>
2021-07-22 02:00:50 +0530

    added border to image and made online prop optional

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,26 +1,28 @@
 import React, { ImgHTMLAttributes } from "react";
 
 interface Props extends ImgHTMLAttributes<HTMLImageElement> {
-  online: boolean;
+  online?: boolean;
   src: string;
 }
 
-const Avatar: React.FC<Props> = ({ online, src, ...rest }) => {
+const Avatar: React.FC<Props> = ({ online, src, className, ...rest }) => {
   const statusClass = online === true ? "bg-green-600" : "bg-gray-400";
   return (
-    <button className="relative">
+    <button className={"relative " + className}>
       <img
         {...rest}
         src={src}
         alt="profile"
-        className="w-16 h-16 rounded-full"
+        className="object-cover w-16 h-16 border-2 border-white rounded-full "
       />
-      <div
-        className={
-          "absolute bottom-0 w-4 h-4 border-2 border-white rounded-full right-2 " +
-          statusClass
-        }
-      ></div>
+      {online && (
+        <div
+          className={
+            "absolute bottom-0 w-4 h-4 border-2 border-white rounded-full right-2 " +
+            statusClass
+          }
+        ></div>
+      )}
     </button>
   );
 };
