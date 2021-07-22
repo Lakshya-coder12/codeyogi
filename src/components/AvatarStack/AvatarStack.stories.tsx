b549b1c43940b1945630dfd83a8cf9121d4cd585
Lakshya Kumar <lkkumar544@gmail.com>
2021-07-22 14:11:20 +0530

    added story for AvatarStack component

---
+++ src/components/AvatarStack/AvatarStack.stories.tsx
@@ -0,0 +1,23 @@
+import AvatarStack from "./AvatarStack";
+
+const avatarsURL = [
+  "https://images.unsplash.com/photo-1612299065617-f883adb67bd1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
+  "https://images.unsplash.com/photo-1615109398623-88346a601842?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+  "https://images.unsplash.com/photo-1560182984-6cc9a59bb4f7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=332&q=80",
+  "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80",
+  "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
+  "https://images.unsplash.com/photo-1489980869433-d1f7c7ac0fcf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+  "https://images.unsplash.com/photo-1561804752-e467ed4cd52a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+  "https://images.unsplash.com/photo-1568990545613-aa37e9353eb6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80",
+];
+
+export default {
+  title: "AvatarStack",
+  component: AvatarStack,
+};
+
+export const main = (args: any) => <AvatarStack {...args}></AvatarStack>;
+
+main.args = {
+  avatars: avatarsURL,
+};
