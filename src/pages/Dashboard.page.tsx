595b33c0237b004af40f5a6fccf381ec3f90a84a
Lakshya Kumar <lkkumar544@gmail.com>
2021-07-26 10:21:53 +0530

    added GroupListItem for displaying list of groups returned from APi

--- src/pages/Dashboard.page.tsx
+++ src/pages/Dashboard.page.tsx
@@ -1,15 +1,70 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { fetchGroups, GroupResponseElement } from "../api";
+import { FiSearch } from "react-icons/fi";
+import Input from "../components/Input/Input";
+import GroupListItem from "../components/GroupListItem";
+import Button from "../components/Button/Button";
 
 interface Props {}
 
 const Dashboard: React.FC<Props> = (props) => {
+  const [query, setQuery] = useState("");
+  const [inputData, setInputData] = useState("");
+  const [data, setData] = useState<GroupResponseElement[] | void>([]);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedData = await fetchGroups({
+        status: "all-groups",
+        query,
+      }).then((result) => {
+        return result;
+      });
+      console.log(fetchedData);
+      setData(fetchedData);
+    }
+    fetchData();
+  }, [query]);
   return (
-    <div>
-      This is Dashboard page.
-      <Link to="/recordings">
-        <span className="text-blue-500">Go to recordings</span>
-      </Link>
+    <div className="flex items-center px-10 space-x-10">
+      <div className="flex-1">
+        <Input
+          Icon={FiSearch}
+          placeholder="Type Group name"
+          className="mt-8"
+          onChange={handleChange}
+        />
+        {data &&
+          data.map((element, index) => (
+            <GroupListItem
+              imgURL={element.group_img_url}
+              groupTitle={element.name}
+              key={index}
+            />
+          ))}
+      </div>
+      <div className="flex-1">
+        <div className="flex items-center">
+          <Input
+            Icon={FiSearch}
+            placeholder="Type Group name"
+            className="mt-8"
+            onChange={(e) => setInputData(e.target.value)}
+          />
+          <Button onClick={() => setQuery(inputData)}>Find</Button>
+        </div>
+        {data &&
+          data.map((element, index) => (
+            <GroupListItem
+              imgURL={element.group_img_url}
+              groupTitle={element.name}
+              key={index}
+            />
+          ))}
+      </div>
     </div>
   );
 };
