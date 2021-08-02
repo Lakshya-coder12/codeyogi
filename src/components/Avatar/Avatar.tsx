2f1746553186856e705a8b3583207ff77d36a625
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-03 01:41:51 +0530

    added new props

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -3,7 +3,9 @@ import React, { ImgHTMLAttributes } from "react";
 interface Props extends ImgHTMLAttributes<HTMLImageElement> {
   online?: boolean;
   requireStatus?: boolean;
+  rounded?: boolean;
   src: string;
+  imgSize?: "small" | "medium" | "large";
 }
 
 const Avatar: React.FC<Props> = ({
@@ -11,21 +13,45 @@ const Avatar: React.FC<Props> = ({
   src,
   className,
   requireStatus,
+  imgSize,
+  rounded,
   ...rest
 }) => {
+  const imgShape = rounded ? "rounded-full " : "rounded-md ";
   const statusClass = online === true ? "bg-green-600" : "bg-gray-400";
+  let statusSizeClass = "";
+  if (requireStatus) {
+    if (imgSize === "small") {
+      statusSizeClass = "w-2 h-2 right-1 ";
+    } else if (imgSize === "medium") {
+      statusSizeClass = "w-3 h-3 right-3 ";
+    } else {
+      statusSizeClass = "w-4 h-4 right-4 ";
+    }
+  }
+  let imgSizeClass = "";
+  if (imgSize === "small") {
+    imgSizeClass = "w-8 h-8";
+  } else if (imgSize === "medium") {
+    imgSizeClass = "w-16 h-16";
+  } else {
+    imgSizeClass = "w-28 h-28";
+  }
   return (
     <button className={"relative " + className}>
       <img
         {...rest}
         src={src}
         alt="profile"
-        className="object-cover w-16 h-16 border-2 border-white rounded-full "
+        className={
+          "object-cover border-2 border-white " + imgShape + imgSizeClass
+        }
       />
       {requireStatus && (
         <div
           className={
-            "absolute bottom-0 w-4 h-4 border-2 border-white rounded-full right-2 " +
+            "absolute bottom-0 border-2 border-white rounded-full " +
+            statusSizeClass +
             statusClass
           }
         ></div>
@@ -37,6 +63,9 @@ const Avatar: React.FC<Props> = ({
 Avatar.defaultProps = {
   online: false,
   requireStatus: false,
+  imgSize: "medium",
+  className: "",
+  rounded: true,
 };
 
 export default React.memo(Avatar);
