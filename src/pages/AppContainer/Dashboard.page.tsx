0e693c9e9272e0780a11dd4f24af6088ec986001
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-03 01:56:39 +0530

    used redux for state management

--- src/pages/AppContainer/Dashboard.page.tsx
+++ src/pages/AppContainer/Dashboard.page.tsx
@@ -3,16 +3,14 @@ import { useEffect } from "react";
 import { fetchGroups } from "../../api/groups";
 import { GroupResponseElement } from "../../models/Groups";
 import { FiSearch } from "react-icons/fi";
-import Input from "../../components/Input/Input";
 import GroupListItem from "../../components/GroupListItem";
 import Button from "../../components/Button/Button";
-import { User } from "../../models/User";
+import { useAppSelector } from "../../store";
 
-interface Props {
-  user: User;
-}
+interface Props {}
 
-const Dashboard: React.FC<Props> = ({ user }) => {
+const Dashboard: React.FC<Props> = (props) => {
+  const user = useAppSelector((state) => state.me);
   const [query, setQuery] = useState("");
   const [inputData, setInputData] = useState("");
   const [data, setData] = useState<GroupResponseElement[] | void>([]);
@@ -28,17 +26,21 @@ const Dashboard: React.FC<Props> = ({ user }) => {
     <div>
       <div className="mx-10 my-6 bg-gray-100 rounded-lg">
         <div className="p-5 text-2xl font-semibold text-gray-700">
-          {`Welcome! ${user.first_name} ${user.middle_name} ${user.last_name}`}
+          {`Welcome! ${user!.first_name} ${user!.middle_name} ${
+            user!.last_name
+          }`}
         </div>
       </div>
       <div className="flex items-center px-10 space-x-10">
         <div className="flex-1">
-          <Input
-            Icon={FiSearch}
-            placeholder="Type Group name"
-            className="mt-8"
-            onChange={handleChange}
-          />
+          <div className="flex items-center p-2 mb-6 bg-white border border-gray-600 rounded-md">
+            <FiSearch className="w-6 h-6 text-blue-600" />
+            <input
+              className="w-full pl-4 placeholder-gray-300 focus:outline-none"
+              placeholder="Type Group Name"
+              onChange={handleChange}
+            />
+          </div>
           {data &&
             data.map((element, index) => (
               <GroupListItem
@@ -49,13 +51,15 @@ const Dashboard: React.FC<Props> = ({ user }) => {
             ))}
         </div>
         <div className="flex-1">
-          <div className="flex items-center">
-            <Input
-              Icon={FiSearch}
-              placeholder="Type Group name"
-              className="mt-8"
-              onChange={(e) => setInputData(e.target.value)}
-            />
+          <div className="flex space-x-6">
+            <div className="flex items-center p-2 mb-6 bg-white border border-gray-600 rounded-md">
+              <FiSearch className="w-6 h-6 text-blue-600" />
+              <input
+                className="w-full pl-4 placeholder-gray-300 focus:outline-none"
+                placeholder="Type Group Name"
+                onChange={(e) => setInputData(e.target.value)}
+              />
+            </div>
             <Button onClick={() => setQuery(inputData)}>Find</Button>
           </div>
           {data &&
