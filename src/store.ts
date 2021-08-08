fb33c6d4cacea98566a97d3e7a68beb32901ef0d
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-08 10:39:27 +0530

    separated reducers of different entities

--- src/store.ts
+++ src/store.ts
@@ -1,64 +1,20 @@
 import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { AnyAction, createStore, Reducer } from "redux";
-import { GroupResponseElement } from "./models/Groups";
-import { User } from "./models/User";
+import { combineReducers, createStore } from "redux";
+import { authReducer } from "./reducers/auth.reducer";
+import { groupReducer } from "./reducers/groups.reducer";
+import { userReducer } from "./reducers/user.reducer";
 
-const ME_FETCH = "me/fetch";
-const ME_LOGIN = "me/login";
-export const GROUPS_QUERY = "groups/query";
-export const GROUPS_QUERY_COMPLETED = "groups/query_completed";
-
-export interface AppState {
-  me?: User;
-  isSidebarOpen: boolean;
-  groupQuery: string;
-  groupQueryMap: { [query: string]: number[] };
-  groups: { [id: number]: GroupResponseElement };
-}
-
-const initialState: AppState = {
-  me: undefined,
-  isSidebarOpen: true,
-  groupQuery: "",
-  groupQueryMap: {},
-  groups: {},
-};
-
-const reducer: Reducer<AppState, AnyAction> = (
-  state = initialState,
-  action: AnyAction
-) => {
-  switch (action.type) {
-    case ME_FETCH:
-    case ME_LOGIN:
-      return { ...state, me: action.payload };
-    case GROUPS_QUERY:
-      return { ...state, groupQuery: action.payload };
-    case GROUPS_QUERY_COMPLETED:
-      const groups = action.payload.groups as GroupResponseElement[];
-      const groupIds = groups.map((g) => g.id);
-      const groupMap = groups.reduce((prev, group) => {
-        return { ...prev, [group.id]: group };
-      }, {});
-      return {
-        ...state,
-        groupQueryMap: {
-          ...state.groupQueryMap,
-          [action.payload.query]: groupIds,
-        },
-        groups: { ...state.groups, ...groupMap },
-      };
-    default:
-      return state;
-  }
-};
+const reducer = combineReducers({
+  auth: authReducer,
+  users: userReducer,
+  groups: groupReducer,
+});
 
 export const store = createStore(
   reducer,
-  window._REDUX__DEVTOOLS_EXTENSION_ && window._REDUX__DEVTOOLS_EXTENSION_()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-export const meFetchAction = (u: User) => ({ type: ME_FETCH, payload: u });
-export const meLoginAction = (u: User) => ({ type: ME_LOGIN, payload: u });
+export type AppState = ReturnType<typeof reducer>;
 
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
