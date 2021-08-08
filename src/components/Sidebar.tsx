464e07d9f1160e9038349ad5a65a61eba647aacc
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-08 21:53:25 +0530

    added link to dashboard and Groups page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SidebarLink from "./SidebarLink";
 import { FiCpu, FiHome } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 interface Props {}
 
@@ -8,8 +9,12 @@ const Sidebar: React.FC<Props> = (props) => {
   const svgClass = "w-5 h-5";
   return (
     <div className="pl-2.5 pt-2.5 pr-5 mt-5">
-      <SidebarLink title="Dashboard" svg={<FiHome className={svgClass} />} />
-      <SidebarLink title="Home" svg={<FiCpu className={svgClass} />} />
+      <Link to="/dashboard">
+        <SidebarLink title="Dashboard" svg={<FiHome className={svgClass} />} />
+      </Link>
+      <Link to="/groups">
+        <SidebarLink title="Groups" svg={<FiCpu className={svgClass} />} />
+      </Link>
     </div>
   );
 };
