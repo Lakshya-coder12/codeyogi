86ce8f9592bc959743e1a9a98f37fd4458eecf0d
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-09 20:15:33 +0530

    add file for action constants

---
+++ src/actions/actions.constants.ts
@@ -0,0 +1,7 @@
+//Auth
+export const ME_FETCH = "me/fetch";
+export const ME_LOGIN = "me/login";
+
+//Groups
+export const GROUPS_QUERY = "groups/query";
+export const GROUPS_QUERY_COMPLETED = "groups/query_completed";
