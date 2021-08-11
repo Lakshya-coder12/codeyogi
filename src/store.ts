44b0dd7e82bbca3f7501f7e46f47a4a35b336880
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-11 11:01:44 +0530

    used saga middleware

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,11 @@
 import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { authReducer } from "./reducers/auth.reducer";
 import { groupReducer } from "./reducers/groups.reducer";
 import { userReducer } from "./reducers/user.reducer";
+import { sagaMiddleware } from "./sagas";
+import { watchGroupQueryChanged } from "./sagas/groups.sagas";
 
 const reducer = combineReducers({
   auth: authReducer,
@@ -12,8 +15,10 @@ const reducer = combineReducers({
 
 export const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
+
+sagaMiddleware.run(watchGroupQueryChanged);
 
 export type AppState = ReturnType<typeof reducer>;
 
