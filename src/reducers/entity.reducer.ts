dd6a2b6a736a76493a12abe6aa5296c64cc251ac
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-15 23:52:51 +0530

    added reducer for selecting a group and setting error

--- src/reducers/entity.reducer.ts
+++ src/reducers/entity.reducer.ts
@@ -4,14 +4,47 @@ export interface EntityState<T extends Entity = Entity> {
   byID: {
     [id: number]: T;
   };
+  selectedID?: number;
+  loadingOne: boolean;
+  loadingList: boolean;
+  errorOne?: string;
 }
+
+export const initialEntityState = {
+  byID: {},
+  loadingOne: false,
+  loadingList: false,
+};
 
 export const getIDs = (entities: Entity[]) => {
   return entities.map((e) => e.id);
 };
 
-export const addOne = (state: EntityState, entity: Entity) => {
-  return { ...state, byID: { ...state.byID, [entity.id]: entity } };
+export const select = (state: EntityState, id: number) => ({
+  ...state,
+  selectedID: id,
+  loadingOne: true,
+  errorOne: undefined,
+});
+
+export const setErrorForOne = (state: EntityState, id: number, msg: string) => {
+  if (state.selectedID !== id) {
+    return state;
+  }
+  return { ...state, errorOne: msg, loadingOne: false };
+};
+
+export const addOne = (
+  state: EntityState,
+  entity: Entity,
+  loading?: boolean
+) => {
+  const newLoading = loading === undefined ? state.loadingOne : loading;
+  return {
+    ...state,
+    byID: { ...state.byID, [entity.id]: entity },
+    loadingOne: newLoading,
+  };
 };
 
 export const addMany = (state: EntityState, entities: Entity[]) => {
