c737140cc775c1e8ed188c61406237b27f5fddf2
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-15 13:50:06 +0530

    added more fields

--- src/models/Groups.ts
+++ src/models/Groups.ts
@@ -1,6 +1,9 @@
 import { Entity } from "./Entity";
+import { User } from "./User";
 
 export interface Group extends Entity {
   name: string;
   group_img_url: string;
+  description: string;
+  creator: User;
 }
