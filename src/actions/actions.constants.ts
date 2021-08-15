ee5ed77c841617ee1750de7a0931933cf4927ce2
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-15 23:20:36 +0530

    added constant for fetch one error

--- src/actions/actions.constants.ts
+++ src/actions/actions.constants.ts
@@ -5,5 +5,7 @@ export const ME_LOGIN = "me/login";
 //Groups
 export const GROUP_FETCH_ONE = "groups/fetch_one";
 export const GROUP_FETCH_ONE_COMPLETED = "groups/fetch_one_completed";
+export const GROUP_FETCH_ONE_ERROR = "groups/fetch_one_error";
+
 export const GROUPS_QUERY_CHANGED = "groups/query";
 export const GROUPS_QUERY_COMPLETED = "groups/query_completed";
