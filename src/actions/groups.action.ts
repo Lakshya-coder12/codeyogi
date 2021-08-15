67383a9913a86e276ae4b8bc2520137c45c5de9a
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-15 13:39:41 +0530

    added actions for fetching single group

--- src/actions/groups.action.ts
+++ src/actions/groups.action.ts
@@ -2,6 +2,8 @@ import { Group } from "../models/Groups";
 import {
   GROUPS_QUERY_CHANGED,
   GROUPS_QUERY_COMPLETED,
+  GROUP_FETCH_ONE,
+  GROUP_FETCH_ONE_COMPLETED,
 } from "./actions.constants";
 
 export const queryChangedAction = (query: string) => ({
@@ -11,4 +13,14 @@ export const queryChangedAction = (query: string) => ({
 export const queryCompletedAction = (query: string, groups: Group[]) => ({
   type: GROUPS_QUERY_COMPLETED,
   payload: { query, groups },
+});
+
+export const fetchOneGroup = (id: number) => ({
+  type: GROUP_FETCH_ONE,
+  payload: id,
+});
+
+export const fetchOneGroupCompleted = (group: Group) => ({
+  type: GROUP_FETCH_ONE_COMPLETED,
+  payload: group,
 });
