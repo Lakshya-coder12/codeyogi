e3b96076962487f6d8b1f07fd728a33693e11b71
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-15 14:19:03 +0530

    gave h-screen to outer div

--- src/pages/AppContainer/Dashboard.page.tsx
+++ src/pages/AppContainer/Dashboard.page.tsx
@@ -7,9 +7,13 @@ interface Props {}
 const Dashboard: React.FC<Props> = (props) => {
   const user = useAppSelector(meSelector);
   return (
-    <div className="mx-10 my-6 bg-gray-100 rounded-lg">
-      <div className="p-5 text-2xl font-semibold text-gray-700">
-        {`Welcome! ${user!.first_name} ${user!.middle_name} ${user!.last_name}`}
+    <div className="h-screen">
+      <div className="mx-10 my-6 bg-gray-100 rounded-lg">
+        <div className="p-5 text-2xl font-semibold text-gray-700">
+          {`Welcome! ${user!.first_name} ${user!.middle_name} ${
+            user!.last_name
+          }`}
+        </div>
       </div>
     </div>
   );
