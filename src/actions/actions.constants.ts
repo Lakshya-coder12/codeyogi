8c1788b6b18cd465487c20c354fe5dae3b38e943
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-15 13:38:52 +0530

    added new action constants for fetching single group

--- src/actions/actions.constants.ts
+++ src/actions/actions.constants.ts
@@ -3,5 +3,7 @@ export const ME_FETCH = "me/fetch";
 export const ME_LOGIN = "me/login";
 
 //Groups
+export const GROUP_FETCH_ONE = "groups/fetch_one";
+export const GROUP_FETCH_ONE_COMPLETED = "groups/fetch_one_completed";
 export const GROUPS_QUERY_CHANGED = "groups/query";
 export const GROUPS_QUERY_COMPLETED = "groups/query_completed";
