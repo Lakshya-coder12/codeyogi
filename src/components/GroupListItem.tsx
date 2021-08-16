0f0fc66d9eabe824677fa1a57f90065d732e74c6
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-17 01:38:46 +0530

    changed styling of group list item

--- src/components/GroupListItem.tsx
+++ src/components/GroupListItem.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GroupListItem: React.FC<Props> = ({ imgURL, groupTitle, id }) => {
   return (
-    <div className="flex items-center px-4 py-4 mb-2 bg-white border border-gray-300 rounded-lg space-x-14 hover:bg-gray-300">
+    <div className="flex items-center max-w-2xl px-4 py-4 mx-auto mb-2 bg-white border border-gray-300 rounded-lg space-x-14 hover:bg-gray-300">
       <Avatar src={imgURL} />
       <Link to={"/groups/" + id}>
         <div className="font-semibold text-gray-700">{groupTitle}</div>
