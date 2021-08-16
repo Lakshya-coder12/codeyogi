5257865f791592a4a94bcca4f39825a6fd0fd042
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-17 01:51:38 +0530

    moved common code to root saga

--- src/sagas/groups.sagas.ts
+++ src/sagas/groups.sagas.ts
@@ -1,16 +1,5 @@
-import {
-  call,
-  put,
-  takeLatest,
-  delay,
-  all,
-  takeEvery,
-} from "@redux-saga/core/effects";
+import { call, put, delay } from "@redux-saga/core/effects";
 import { AnyAction } from "redux";
-import {
-  GROUPS_QUERY_CHANGED,
-  GROUP_FETCH_ONE,
-} from "../actions/actions.constants";
 import {
   fetchOneGroupCompleted,
   fetchOneGroupError,
@@ -18,17 +7,7 @@ import {
 } from "../actions/groups.action";
 import { fetchGroups as fetchGroupsAPI, fetchOneGroup } from "../api/groups";
 
-function* fetchGroups(action: AnyAction): Generator<any> {
-  yield delay(300);
-
-  const groupsResponse: any = yield call(fetchGroupsAPI, {
-    query: action.payload,
-    status: "all-groups",
-  });
-  yield put(queryCompletedAction(action.payload, groupsResponse.data.data));
-}
-
-function* fetchOne(action: AnyAction): Generator<any> {
+export function* fetchOne(action: AnyAction): Generator<any> {
   try {
     const response: any = yield call(fetchOneGroup, action.payload);
     yield put(fetchOneGroupCompleted(response.data.data));
@@ -38,9 +17,12 @@ function* fetchOne(action: AnyAction): Generator<any> {
   }
 }
 
-export function* watchGroupQueryChanged() {
-  yield all([
-    takeLatest(GROUPS_QUERY_CHANGED, fetchGroups),
-    takeEvery(GROUP_FETCH_ONE, fetchOne),
-  ]);
+export function* fetchGroups(action: AnyAction): Generator<any> {
+  yield delay(300);
+
+  const groupsResponse: any = yield call(fetchGroupsAPI, {
+    query: action.payload,
+    status: "all-groups",
+  });
+  yield put(queryCompletedAction(action.payload, groupsResponse.data.data));
 }
