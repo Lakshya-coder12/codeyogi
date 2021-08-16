75091cbc628fb08d51ac863601db2ee0305c9161
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-17 01:37:32 +0530

    added actions related to fetching single user

--- src/actions/actions.constants.ts
+++ src/actions/actions.constants.ts
@@ -9,3 +9,11 @@ export const GROUP_FETCH_ONE_ERROR = "groups/fetch_one_error";
 
 export const GROUPS_QUERY_CHANGED = "groups/query";
 export const GROUPS_QUERY_COMPLETED = "groups/query_completed";
+
+//Users
+export const USERS_FETCH = "users/fetch";
+export const USERS_FETCH_COMPLETED = "users/fetch_completed";
+
+export const USERS_FETCH_ONE = "users/fetch_one";
+export const USERS_FETCH_ONE_COMPLETED = "users/fetch_one_completed";
+export const USERS_FETCH_ONE_ERROR = "users/fetch_one_error";
