f7c429c54b4588d488eadf0302122385afd61474
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-17 01:49:16 +0530

    changed styling of loader and group list items

--- src/pages/AppContainer/GroupList.page.tsx
+++ src/pages/AppContainer/GroupList.page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FaSpinner } from "react-icons/fa";
 import { FiSearch } from "react-icons/fi";
 import { useDispatch } from "react-redux";
 import { queryChangedAction } from "../../actions/groups.action";
@@ -20,53 +19,41 @@ const GroupList: React.FC<Props> = (props) => {
   const groups = useAppSelector(groupSelector);
   const dispatch = useDispatch();
   return (
-    <div className="flex items-center px-10 mt-5 space-x-10">
-      <div className="flex-1">
-        <div className="flex items-center p-2 mb-6 bg-white border border-gray-600 rounded-md">
-          <FiSearch className="w-6 h-6 text-blue-600" />
-          <Input
-            className="w-full pl-4 placeholder-gray-300 focus:outline-none"
-            placeholder="Type Group Name"
-            value={query}
-            onChange={(e) => {
-              dispatch(queryChangedAction(e.target.value));
-            }}
-          />
-          {loading && <FaSpinner className="w-5 h-5 ml-12 animate-spin" />}
-        </div>
-        {groups &&
-          groups.map((group, index) => (
-            <GroupListItem
-              imgURL={group.group_img_url}
-              groupTitle={group.name}
-              key={index}
-              id={group.id}
-            />
-          ))}
+    <div className="px-5">
+      <div className="flex items-center max-w-2xl p-2 mx-auto mt-5 mb-6 bg-white border border-gray-600 rounded-md">
+        <FiSearch className="w-6 h-6 text-blue-600" />
+        <Input
+          className="w-full pl-4 placeholder-gray-300 focus:outline-none"
+          placeholder="Type Group Name"
+          value={query}
+          onChange={(e) => {
+            dispatch(queryChangedAction(e.target.value));
+          }}
+        />
       </div>
-      {/* <div className="flex-1">
-          <div className="flex space-x-6">
-            <div className="flex items-center p-2 mb-6 bg-white border border-gray-600 rounded-md">
-              <FiSearch className="w-6 h-6 text-blue-600" />
-              <input
-                className="w-full pl-4 placeholder-gray-300 focus:outline-none"
-                placeholder="Type Group Name"
-                onChange={(e) => {
-                  dispatch({ type: GROUPS_QUERY, payload: e.target.value });
-                }}
-              />
+      {loading && (
+        <div className="w-full max-w-2xl p-4 mx-auto mt-10 mb-2 border border-gray-300 rounded-md shadow">
+          <div className="flex space-x-4 animate-pulse">
+            <div className="w-12 h-12 bg-gray-400 rounded-full"></div>
+            <div className="flex-1 py-1 space-y-4">
+              <div className="w-3/4 h-4 bg-gray-400 rounded"></div>
+              <div className="space-y-2">
+                <div className="h-4 bg-gray-400 rounded"></div>
+                <div className="w-5/6 h-4 bg-gray-400 rounded"></div>
+              </div>
             </div>
-            <Button>Find</Button>
           </div>
-          {groups &&
-            groups.map((element, index) => (
-              <GroupListItem
-                imgURL={element.group_img_url}
-                groupTitle={element.name}
-                key={index}
-              />
-            ))}
-        </div> */}
+        </div>
+      )}
+      {groups &&
+        groups.map((group, index) => (
+          <GroupListItem
+            imgURL={group.group_img_url}
+            groupTitle={group.name}
+            key={index}
+            id={group.id}
+          />
+        ))}
     </div>
   );
 };
