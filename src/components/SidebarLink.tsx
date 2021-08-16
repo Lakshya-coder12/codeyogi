48f0c629bc10641805349e69fd5f836b5e069908
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-17 01:43:30 +0530

    extended props interface with Button attributes

--- src/components/SidebarLink.tsx
+++ src/components/SidebarLink.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { ButtonHTMLAttributes } from "react";
 import { FiChevronRight } from "react-icons/fi";
 
-interface Props {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   svg: React.ReactElement;
 }
 
 const SidebarLink: React.FC<Props> = ({ title, svg }) => {
   return (
-    <button className="flex px-3.5 py-2.75 mb-1 hover:bg-gray-300 rounded-lg items-center w-full bg-white">
+    <div className="flex px-3.5 py-2.75 mb-1 hover:bg-gray-300 rounded-lg items-center w-full bg-white">
       <div className="mr-3">{svg}</div>
       <div className="font-semibold text-13">{title}</div>
       <div className="flex-1">
         <FiChevronRight className="w-4 h-4 ml-auto" />
       </div>
-    </button>
+    </div>
   );
 };
 
