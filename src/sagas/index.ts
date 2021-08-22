10151eb96937dc45dd9d4b986482f6bcce5909f6
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-22 09:29:34 +0530

    implemented rootSaga function

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,3 +1,11 @@
+import { fork } from "@redux-saga/core/effects";
 import createSagaMiddleware from "redux-saga";
+import { watchGroupActions } from "./groups.sagas";
+import { watchUserActions } from "./users.sagas";
 
 export const sagaMiddleware = createSagaMiddleware();
+
+export function* rootSaga() {
+  yield fork(watchGroupActions);
+  yield fork(watchUserActions);
+}
