9a6b9a8692b333e62e475874c644ec1c346c977e
Lakshya Kumar <lkkumar544@gmail.com>
2021-08-23 17:35:44 +0530

    created userListReceived action

--- src/actions/users.actions.ts
+++ src/actions/users.actions.ts
@@ -5,6 +5,7 @@ import {
   USERS_FETCH_ONE,
   USERS_FETCH_ONE_COMPLETED,
   USERS_FETCH_ONE_ERROR,
+  USER_LIST_RECEIVED,
 } from "./actions.constants";
 
 export const fetchUsers = () => ({
@@ -29,4 +30,9 @@ export const fetchOneUserCompleted = (user: User) => ({
 export const fetchOneUserError = (id: number, msg: string) => ({
   type: USERS_FETCH_ONE_ERROR,
   payload: { id, msg },
+});
+
+export const userListReceived = (usersByID: { [id: number]: User }) => ({
+  type: USER_LIST_RECEIVED,
+  payload: usersByID,
 });
